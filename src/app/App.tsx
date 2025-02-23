bf6491600baa1d3cd09f4a8879fac5c8d4313178
“alexkior” <alex.n.kior@gmail.com>
2025-02-23 02:08:05 +0100

    Add header for the home page and bottom menu for the whole app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,13 +2,35 @@ import { NavigationContainer } from '@react-navigation/native'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 import { Application } from './ui'
+import { StyleSheet, View } from 'react-native'
 
 export function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
         <Application />
+        <View
+          style={styles.bottomContainer}
+          >
+          
+        </View>
       </NavigationContainer>
     </SafeAreaProvider>
   )
 }
+
+const styles = StyleSheet.create({
+  bottomContainer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: '#fff',
+    width: '100%',
+    height: 100,
+    // TODO: remove shadow
+    boxShadow: '0px -4px 3px rgba(23, 23, 23, 0.1)'
+  }
+})
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,15 +1,33 @@
 import { StyleSheet, View } from 'react-native'
 
 export const HomePage: React.FC = () => {
-  return <View style={styles.container}></View>
+  return (
+    <View style={styles.container}>
+      <View style={styles.headerContainer}></View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#8be8f1',
+    backgroundColor: '#f9f9f9',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  headerContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: '#fff',
+    width: '100%',
+    height: 100,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+    boxShadow: '0px 4px 3px rgba(23, 23, 23, 0.1)'
   }
 })

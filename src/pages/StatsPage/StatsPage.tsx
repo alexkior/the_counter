2f65bc3d6b6f3cb3ecbbb4b162f22618a36b47f0
“alexkior” <alex.n.kior@gmail.com>
2025-03-03 02:40:35 +0100

    Create week day frequancy calculation logic

--- src/pages/StatsPage/StatsPage.tsx
+++ src/pages/StatsPage/StatsPage.tsx
@@ -14,13 +14,42 @@ export const StatsPage: React.FC = observer(() => {
   const startDate = daysStore.getDays()[0]
   const today = new Date().toISOString().split('T')[0]
   const daysPassed = startDate ? Math.floor((Date.parse(today) - Date.parse(startDate)) / 86400000) + 1 : 0
+  const weekDaysPassed = countWeekdaysSince(startDate)
   const selectedDates = daysStore.getDays().length
+  const selectedWeekDays = daysStore.getDays().reduce(
+    (acc: { [key: number]: number }, date: string) => {
+      const day = new Date(date).getDay()
+      acc[day] = acc[day] ? acc[day] + 1 : 1
+      return acc
+    },
+    { '0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0 }
+  )
+
   const percentage = selectedDates
     ? selectedDates >= daysPassed
       ? 100
       : Math.floor((selectedDates / daysPassed) * 100)
     : 0
   console.log(percentage, daysPassed, selectedDates)
+
+  function countWeekdaysSince(dateStr: string): Record<string, number> {
+    const startDate = new Date(dateStr)
+    const endDate = new Date()
+
+    const daysArray: Date[] = []
+    for (let d = new Date(startDate); d <= endDate; d.setDate(d.getDate() + 1)) {
+      daysArray.push(new Date(d))
+    }
+
+    return daysArray.reduce<Record<string, number>>(
+      (acc, date) => {
+        const weekday = date.getDay().toString()
+        acc[weekday] = (acc[weekday] || 0) + 1
+        return acc
+      },
+      { '0': 0, '1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0 }
+    )
+  }
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
@@ -53,18 +82,14 @@ export const StatsPage: React.FC = observer(() => {
       </View>
       <View style={styles.box}>
         <View style={styles.progressCircleWrapper}>
-          <CircularProgress value={percentage} color={theme.colors.red} />
-          <CircularProgress value={percentage} color={theme.colors.red} />
-
-          <CircularProgress value={percentage} color={theme.colors.red} />
-
-          <CircularProgress value={percentage} color={theme.colors.red} />
-
-          <CircularProgress value={percentage} color={theme.colors.red} />
-
-          <CircularProgress value={percentage} color={theme.colors.red} />
-
-          <CircularProgress value={percentage} color={theme.colors.red} />
+          {Object.entries(selectedWeekDays).map((day, index) => (
+            <View key={index}>
+              <CircularProgress
+                value={Math.floor((weekDaysPassed[index] / 100) * day[1] * 100)}
+                color={theme.colors.red}
+              />
+            </View>
+          ))}
         </View>
       </View>
       <View style={styles.box}></View>

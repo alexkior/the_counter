9943a7ee68f73710e462f774c295c5cc47734e94
“alexkior” <alex.n.kior@gmail.com>
2025-03-19 21:40:21 +0100

    Refactor year calendar view

--- src/pages/CalendarPage/components/YearCalendar/YearCalendar.tsx
+++ src/pages/CalendarPage/components/YearCalendar/YearCalendar.tsx
@@ -1,5 +1,5 @@
 import { memo } from 'react'
-import { View, ScrollView, Dimensions } from 'react-native'
+import { View, ScrollView } from 'react-native'
 import { Calendar } from 'react-native-calendars'
 
 import { YearDay, YearHeader } from '../'
@@ -8,29 +8,17 @@ import { useStyles } from './useStyles'
 export const YearCalendar: React.FC = memo(() => {
   const months = Array.from({ length: 12 }, (_, i) => i + 1)
   const year = new Date().getFullYear()
-  const { width } = Dimensions.get('window')
   const { styles } = useStyles()
 
   return (
-    <ScrollView
-      style={{
-        height: '100%',
-        paddingVertical: 120,
-        backgroundColor: 'red',
-        display: 'flex',
-        flexDirection: 'column'
-      }}
-    >
+    <ScrollView style={styles.wrapper}>
       <View style={styles.container}>
         {months.map((month) => {
           const monthStr = month < 10 ? `0${month}` : month.toString()
           return (
             <View
               key={month}
-              style={{
-                width: (width - 20) / 3,
-                marginBottom: 20
-              }}
+              style={styles.calendarWrapper}
             >
               <Calendar
                 displayLoadingIndicator
--- src/pages/CalendarPage/components/YearCalendar/useStyles.ts
+++ src/pages/CalendarPage/components/YearCalendar/useStyles.ts
@@ -1,13 +1,27 @@
-import { StyleSheet } from 'react-native'
+import { Dimensions, StyleSheet } from 'react-native'
 
 export const useStyles = () => {
+  const { width } = Dimensions.get('window')
+
   return {
     styles: StyleSheet.create({
+      wrapper: {
+        paddingTop: 120,
+        paddingBottom: 220,
+        position: 'absolute',
+        top: 0,
+        display: 'flex',
+        flexDirection: 'column'
+      },
       container: {
         flexDirection: 'row',
         flexWrap: 'wrap',
         justifyContent: 'space-between',
         paddingHorizontal: 5
+      },
+      calendarWrapper: {
+        width: (width - 20) / 3,
+        marginBottom: 20
       }
     })
   }
